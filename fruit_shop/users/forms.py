2148a8f539682398c8373565b06fdedf40263b2d
Diogo Gomes <diogo.gomes77@gmail.com>
2020-07-24 23:19:20 +0100

    added user creation and login tests

--- fruit_shop/users/forms.py
+++ fruit_shop/users/forms.py
@@ -7,6 +7,12 @@ class ShopUserCreationForm(UserCreationForm):
         model = get_user_model()
         fields = ('username', 'email', 'password1', 'password2')
 
+    def __init__(self, *args, **kwargs):
+        super(ShopUserCreationForm, self).__init__(*args, **kwargs)
+        # force email filed in
+        # TODO: enforce email not null in model instead of in form
+        self.fields['email'].required = True
+
 
 class ShopUserChangeForm(UserChangeForm):
     class Meta:
--- fruit_shop/users/tests.py
+++ fruit_shop/users/tests.py
@@ -1,3 +1,81 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
 
-# Create your tests here.
+from users.models import ShopUser
+
+
+class UserLoginTest(TestCase):
+    def setUp(self):
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'secret'}
+        ShopUser.objects.create_user(**self.credentials)
+
+    def test_login_success(self):
+        response = self.client.post(
+            reverse_lazy('login'),
+            self.credentials,
+            follow=True)
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue(response.context['user'].is_active)
+
+    def test_login_fail(self):
+        bad_credentials = {
+            'username': 'testuser',
+            'password': 'secret2'}
+        response = self.client.post(
+            reverse_lazy('login'),
+            bad_credentials,
+            follow=True)
+        self.assertEquals(response.status_code, 200)
+        self.assertFalse(response.context['user'].is_active)
+
+    def test_logout(self):
+        self.test_login_success()
+        response = self.client.get(reverse_lazy('logout'))
+        self.assertFalse(response.context['user'].is_active)
+
+
+class UserCreationTest(TestCase):
+    def setUp(self):
+        self.credentials = {
+            'username': 'testuser',
+            'password1': 'secret',
+            'password2': 'secret',
+            'email': 'test@test.test'
+        }
+
+    def test_user_creation_sucess(self):
+        response = self.client.post(
+            reverse_lazy('signup'),
+            self.credentials,
+            follow=True
+        )
+        self.assertEquals(response.status_code, 200)
+        self.assertRedirects(response, reverse_lazy('login'))
+
+    def test_user_creation_email_sucess(self):
+        # test the user registered with email
+        response = self.client.post(
+            reverse_lazy('signup'),
+            self.credentials,
+            follow=True
+        )
+        self.assertEquals(response.status_code, 200)
+        self.assertRedirects(response, reverse_lazy('login'))
+        user = ShopUser.objects.get(username=self.credentials['username'])
+        self.assertIsInstance(user, ShopUser)
+        self.assertNotEqual(user.email, '')
+
+    def test_user_creation_email_fail(self):
+        # test the user registered without email
+        self.credentials['email'] = ''
+        response = self.client.post(
+            reverse_lazy('signup'),
+            self.credentials,
+            follow=True
+        )
+        self.assertEquals(response.status_code, 200)
+        users_qs = ShopUser.objects.filter(username=self.credentials['username'])
+        self.assertEqual(len(users_qs), 0)
