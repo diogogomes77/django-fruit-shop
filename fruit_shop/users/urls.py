e668e21722b2662bc0d96c0f5491a146c3b42c4a
Diogo Gomes <diogo.gomes77@gmail.com>
2020-07-25 17:21:05 +0100

    fixed logout redirection

--- fruit_shop/users/urls.py
+++ fruit_shop/users/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path, include
-
+from django.contrib.auth import views as auth_views
 from users import views
 
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
     path('login/', views.Login.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include('django.contrib.auth.urls')),
 ]
